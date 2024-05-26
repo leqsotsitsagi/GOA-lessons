cd5a0899a8443767d7ef85e2a31c88c79b24abdd
leqsotsitsagi <leqso2007progamer@gmail.com>
2024-05-27 00:10:54 +0400

    commit 3

---
+++ GOA lessons/level 03/classwork/classwork.py
@@ -0,0 +1,30 @@
+num1 = (20)
+num2 = (5)
+num3 = (2.5)
+
+print(num1 + num2)
+print(num1 + num3)
+print(num2 * num1)
+print(num2 * num3)
+print(num1 / num3)
+print(num2 / num3)
+print(num1 / num2)
+print(num1 / num3 * num2)
+print(num2 / num3 + num1)
+print(num1 - num2)
+print(num2 - num3)
+print(num1 - num2 - num3)
+
+name1 = "leqso"
+name2 = "tsitsagi"
+print(name1 + " " + name2)
+
+item1 = (10)
+item2 = (0.5)
+name = ("leqso")
+is_true = True
+
+type(item1)
+type(item2)
+type(name)
+type(is_true)
